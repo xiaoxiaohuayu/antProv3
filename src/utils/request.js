34c58a2028118a151e879aef66c20f806451f92e
haozexiang <17600155201@163.com>
2020-08-18 16:32:26 +0800

    no message

---
+++ src/utils/request.js
@@ -0,0 +1,77 @@
+import axios from 'axios'
+import store from '@/store'
+import storage from 'store'
+import notification from 'ant-design-vue/es/notification'
+import { VueAxios } from './axios'
+import { ACCESS_TOKEN } from '@/store/mutation-types'
+
+// 创建 axios 实例
+const request = axios.create({
+  // API 请求的默认前缀
+  baseURL: process.env.VUE_APP_BASEURL,
+  timeout: 6000,// 请求超时时间
+  withCredentials:true,
+})
+
+//参数序列化
+
+// 异常拦截处理器
+const errorHandler = (error) => {
+  if (error.response) {
+    const data = error.response.data
+    // 从 localstorage 获取 token
+    const token = storage.get(ACCESS_TOKEN)
+    if (error.response.status === 403) {
+      notification.error({
+        message: 'Forbidden',
+        description: data.message
+      })
+    }
+    if (error.response.status === 401 && !(data.result && data.result.isLogin)) {
+      notification.error({
+        message: 'Unauthorized',
+        description: 'Authorization verification failed'
+      })
+      if (token) {
+        store.dispatch('Logout').then(() => {
+          setTimeout(() => {
+            window.location.reload()
+          }, 1500)
+        })
+      }
+    }
+  }
+  return Promise.reject(error)
+}
+
+// request interceptor
+request.interceptors.request.use(config => {
+  console.log(config)
+  const token = storage.get(ACCESS_TOKEN)
+  // 如果 token 存在
+  // 让每个请求携带自定义 token 请根据实际情况自行修改
+  if (token) {
+    config.headers['Access-Token'] = token
+  }
+  return config
+}, errorHandler)
+
+// response interceptor
+request.interceptors.response.use((response) => {
+  console.log(response,'相应拦截')
+  return response.data
+}, errorHandler)
+
+const installer = {
+  vm: {},
+  install (Vue) {
+    Vue.use(VueAxios, request)
+  }
+}
+
+export default request
+
+export {
+  installer as VueAxios,
+  request as axios
+}
---
+++ src/views/map/index.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        一张图管理
+    </div>
+</template>
+<script>
+export default {
+    name:'map',
+    filters:{},
+    components: {},
+    mixins:[],
+    data() {
+        return {
+            
+        }
+    },
+    computed: {},
+    watch: {
+            
+    },
+    beforeCreate() {},
+    created() {
+    },
+    beforeMount() {},
+    mounted() {
+    },
+    beforeUpdate() {},
+    updated() {}, 
+    beforeDestroy() {},
+    destroyed() {},
+    activated() {},
+    methods: {
+            
+    }
+}
+</script>
+<style lang='sass' scoped>
+</style>
